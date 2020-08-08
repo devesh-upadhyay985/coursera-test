9f3bafb2e8390322e2e428d5d5542d550802c931
devesh-upadhyay985 <68326972+devesh-upadhyay985@users.noreply.github.com>
2020-08-08 21:27:36 +0530

    Update SpeakHello.js

--- module-4-solution/harder/SpeakHello.js
+++ module-4-solution/harder/SpeakHello.js
@@ -1,6 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-var speakWord="Hello";
+var speakWords="Hello";
 (function (window){
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
@@ -15,7 +15,7 @@ var speakWord="Hello";
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 		speak: function(name) {
-			console.log(speakWord + " " + name);
+			console.log(speakWords + " " + name);
 		}
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
